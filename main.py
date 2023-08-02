48a0f00ee6766c4cb35f43e6afc1f3b6fc8bde3a
lucas_home <lucasmais@dcc.ufmg.br>
2023-08-02 16:20:43 -0300

    add pages and number

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
-    pdf.add_page()
-    pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], 
-            align="L", ln=1)
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
+    for i in range(row["Pages"]):
+        pdf.add_page()
+        pdf.set_font(family="Times", style="B", size=24)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=180, h=12, txt=row["Topic"], 
+                align="L", ln=0)
+        pdf.cell(w=100, h=12, txt=f"{i+1}", 
+                align="L", ln=1)
+        pdf.line(x1=10, y1=21, x2=200, y2=21)
 
 pdf.output("output2.pdf")
