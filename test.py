58e2b99a50452dd0c8b444072ff3f92bb5db2d1b
lucas_home <lucasmais@dcc.ufmg.br>
2023-08-04 10:01:22 -0300

    Done

--- test.py
+++ test.py
@@ -4,18 +4,18 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 pdf.add_page()
 pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello There!", 
+pdf.cell(w=0, h=12, txt="Hello There!",
          align="L", ln=1, border=1)
 pdf.set_font(family="Times", style="B", size=10)
-pdf.cell(w=0, h=12, txt="Hi There", 
+pdf.cell(w=0, h=12, txt="Hi There",
          align="L", ln=1, border=1)
 
 pdf.add_page()
 pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello There!", 
+pdf.cell(w=0, h=12, txt="Hello There!",
          align="L", ln=1)
 pdf.set_font(family="Times", size=10)
-pdf.cell(w=0, h=12, txt="Hi There", 
+pdf.cell(w=0, h=12, txt="Hi There",
          align="L", ln=1)
 
 pdf.output("output.pdf")
